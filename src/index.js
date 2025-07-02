bf7ace42415fa1d569fba48cf3562f611fae03e5
americo macedo <americomascedo@gmail.com>
2025-07-02 19:53:24 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const produtosRoutes = require('./routes/produtosRoutes');
 const mongoose = require('mongoose');
-require('dotenv').config({path: 'Modulo 11/Atividades/atividade4/src/.env'});
+require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT
